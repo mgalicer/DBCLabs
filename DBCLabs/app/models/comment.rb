b73817f83984782612b39f12e21cf8b3c687b57b
peterhinners & Kilima Glenn <pair+peterhinners.thunderenlight@devbootcamp.com>
2015-08-03 10:11:03 -0500

    Adding the comment model and migration

---
+++ DBCLabs/app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  belongs_to :commenter, class_name: "User", foreign_key: :commenter_id
+  belongs_to :commentable, polymorphic: true
+
+end
--- DBCLabs/app/models/proposal.rb
+++ DBCLabs/app/models/proposal.rb
@@ -1,2 +1,7 @@
 class Proposal < ActiveRecord::Base
+  belongs_to :proposer, class_name: "User", foreign_key: :proposer_id
+
+  has_many :experiments
+  has_many :comments, as: :commentable
+
 end
