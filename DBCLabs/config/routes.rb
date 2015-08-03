dd4d0bcf43abba916d1f434ff9a8143e186514dc
peterhinners & Kilima Glenn <pair+peterhinners.thunderenlight@devbootcamp.com>
2015-08-03 18:13:38 -0500

    profile page in progress

--- DBCLabs/config/routes.rb
+++ DBCLabs/config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-   root 'proposals#index'
+   # root 'proposals#index'
+    root 'welcome#index'
     resources :proposals do
       resources :experiments
     end
-  # root 'welcome#index'
+
 
 
   # Example of regular route:
