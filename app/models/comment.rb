b2173ba7d54afcab5749594b592e2072382519fd
Mari Galicer & Kilima Glenn <pair+mgalicer.thunderenlight@devbootcamp.com>
2015-08-04 14:48:59 -0500

    refactor commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,11 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
 
   def base_commentable
-    commentable = self.commentable
-    while commentable.is_a? Comment
-      commentable = commentable.commentable
-    end
-    commentable
+    self.commentable.try(:base_commentable) || self
   end
 
 end
