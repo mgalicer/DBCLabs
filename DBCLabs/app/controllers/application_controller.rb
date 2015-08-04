e8e83402b1797d709c924423ce14988e94e8de5b
Mari Galicer <you@example.com>
2015-08-03 19:52:42 -0500

    Combined both teams files.

--- DBCLabs/app/controllers/application_controller.rb
+++ DBCLabs/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
