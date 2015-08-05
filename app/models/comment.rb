37003c101b0df7c681f8bb29e214196a9b9f19f1
Kilima Glenn <thunderenlight@gmail.com>
2015-08-05 10:58:32 -0500

    added nesting comments, three comments deep

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commenter, class_name: "User", foreign_key: :commenter_id
   belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
 
   def base_commentable
     self.commentable.try(:base_commentable) || self
